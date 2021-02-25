258aa36aa80a7d7b9940747624e7a3211d851b2e
Nathan Edwards <nathan.edwards@greenroomrobotics.com>
2021-02-26 00:28:44 +1100

    structure now ready for dev

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,5 @@
-/**************************************************************************/
-/*!
-    @file     Adafruit_MMA8451.h
-    @author   K. Townsend (Adafruit Industries)
-    @license  BSD (see license.txt)
-
-    This is an example for the Adafruit MMA8451 Accel breakout board
-    ----> https://www.adafruit.com/products/2019
-
-    Adafruit invests time and resources providing this open source code,
-    please support Adafruit and open-source hardware by purchasing
-    products from Adafruit!
-
-    @section  HISTORY
-
-    v1.0  - First release
-*/
-/**************************************************************************/
+// Jetty Detector
+//    Detector of movement using Arduino and basic accelerometer
 
 #include <Wire.h>
 #include <Adafruit_MMA8451.h>
@@ -28,10 +12,11 @@ Adafruit_MMA8451 mma = Adafruit_MMA8451();
 gyroReading currentReading
 
 void setup(void) {
+
+  // Init Serial
   Serial.begin(9600);
-  
-  Serial.println("Adafruit MMA8451 test!");
-  
+  Serial.println("Starting....");
+
 
   if (! mma.begin()) {
     Serial.println("Couldnt start");
@@ -43,17 +28,20 @@ void setup(void) {
   
   Serial.print("Range = "); Serial.print(2 << mma.getRange());  
   Serial.println("G");
-
-  
   
 }
 
 void loop() {
   
-  getData(currentReading)
-  
+  // Get data
+  while!(getData(currentReading))
+
+  // Write to SD Card
+  while!(writeToSDCard())
 }
 
+// gyroReading_c
+// This outlines structure for data aquisition
 class gyroReading{
   public:
     float x = 0;
@@ -64,8 +52,10 @@ class gyroReading{
     float az = 0;
 }
 
-
-int getData(gyroReading nextReading){
+// getData
+// Uses mma library to get data and store in gyroReading
+// Utilises pointers so only returns finish.
+int getData(gyroReading *nextReading){
   // trigger reading
   mma.read();
   sensors_event_t event; 
@@ -77,8 +67,11 @@ int getData(gyroReading nextReading){
   gyroReading.ay = event.acceleration.y;
   gyroReading.az = event.acceleration.z;
 
+  return 1;
 }
 
 int writeToSDCard( gyroReading myReading ){
   rowString = {String(myReading.x_}
+
+  return 1;
 }
